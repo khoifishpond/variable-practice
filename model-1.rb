88cf0dcdfb73cfd928f87eb884f9591fbd1f7c31
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-17 19:16:33 -0700

    Add hashes

--- model-1.rb
+++ model-1.rb
@@ -1,15 +1,24 @@
+# strings
 name = "Khoi"
 pronouns = "he/him"
 school = "Turing"
 
+# integers and floats
 age = 32
 height = 70.0
 iq = 1000000000000000.0 # LMAO
 
+# booleans
 is_hungry = true
 is_home = true
 in_colorado = false
 
+# arrays
 favorite_colors = ["black", "red"]
 favorite_foods = ["sushi", "ice cream"]
 favorite_movies = ["The Dark Knight", "Interstellar"]
+
+# hashes
+features = {glasses: true, hair: "black", eyes: "black"}
+pets = {dog: "Riley", fish: "OJ"}
+favorites = {colors: favorite_colors, foods: favorite_foods, movies: favorite_movies}
