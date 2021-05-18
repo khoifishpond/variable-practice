8636e0d3eaf29f1ba80c9165f3ae0ab68cd260a3
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-17 19:13:42 -0700

    Add arrays

--- model-1.rb
+++ model-1.rb
@@ -9,3 +9,7 @@ iq = 1000000000000000.0 # LMAO
 is_hungry = true
 is_home = true
 in_colorado = false
+
+favorite_colors = ["black", "red"]
+favorite_foods = ["sushi", "ice cream"]
+favorite_movies = ["The Dark Knight", "Interstellar"]
